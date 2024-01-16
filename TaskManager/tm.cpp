07dcb476d31d898d548824a570f937e83236a7d7
mauposad <mposad9061@gmail.com>
2024-01-15 20:52:37 -0700

    Creating classes as well as variables/ functions for different classes

---
+++ TaskManager/tm.cpp
@@ -0,0 +1,43 @@
+/*Console baseed task management system written in C++
+Author: Mauricio Posadas
+Date Started: January 15th 2024
+
+Current Status: Creating classes as well as variables/ functions for different classes
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Console
+{
+    //vector to store different tasks at beginning
+    //stack or array to store priority on tasks
+    public:
+    //take in different tasks
+    Task addTask(){}
+    void removeTask(){}
+    void markPriority(){}
+
+
+
+};
+
+class Task 
+{
+    string taskDescription;
+    int priorityLevel;
+    bool completed;
+
+
+};
+
+
+int main()
+{
+
+
+    return 0;
+}
